0ddfc3f3293f4e689f22f94e7375f4af73c2bd98
fristicool <74139390+fristicool@users.noreply.github.com>
2021-07-12 17:53:33 +0200

    update server

--- index.js
+++ index.js
@@ -9,23 +9,31 @@ const wss = new WebSocket.Server({ server:server });
 const API_KEY = 'jEcmFUpzZFnioiuyze2S4TerUnVvDNT4'
 var current
 
+let listOfgifs
+
 wss.on('connection', function connection(ws) {
     console.log('new Client')
     ws.on('message', function incoming(message) {
         var returnURL = ''
 
         console.log('received: %s', message);
-        ws.send(message)
         request(`https://api.giphy.com/v1/gifs/search?api_key=jEcmFUpzZFnioiuyze2S4TerUnVvDNT4&q=${message}&limit=25&offset=0&rating=g&lang=en`, { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }
             else {
                 if (body.data.length > 0) {
-                    for(var i = 0; i < body.data.length - 1;i++) {
-                        console.log(body.data[i].images.downsized.url);
-                        returnURL = body.data[i].images.downsized.url
-
-                        ws.send(returnURL)
-                    }
+                    wss.clients.forEach(function each(client) {
+                        returnURL = body.data[0].images.downsized.url
+        
+                        client.send(returnURL)
+                        // if (client.readyState === WebSocket.OPEN) {
+                        //     for(var i = 0; i < body.data.length ;i++) {
+                        //         console.log(body.data[i].images.downsized.url);
+                        //         returnURL = body.data[i].images.downsized.url
+        
+                        //         client.send(returnURL)
+                        //     }
+                        // }
+                    });
                 }
             }
         });
